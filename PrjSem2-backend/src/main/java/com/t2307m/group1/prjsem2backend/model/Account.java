0caf80f1932b8dbc3ef084d225a13ab3841a92ca
hongquan <danghongquanvtc1111@gmail.com>
2024-06-15 12:01:22 +0700

    update model

--- PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/Account.java
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/Account.java
@@ -17,10 +17,63 @@ public class Account {
     private String password;
     private String phoneNumber;// this field require regex
     private String role = "customer"; //default customer, if admin or staff, ghi de
-    private Date createdAt;// khoi tao dung 1 lan
-    private Date updateAt;
-
 
     public Account() {
+    }
+
+    public Account(String userName, String email, String password, String phoneNumber, String role) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
--- PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/ImageCourse.java
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/ImageCourse.java
@@ -14,4 +14,36 @@ public class ImageCourse {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Course course;
+
+    public ImageCourse(String path, Course course) {
+        this.path = path;
+        this.course = course;
+    }
+
+    public ImageCourse() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
