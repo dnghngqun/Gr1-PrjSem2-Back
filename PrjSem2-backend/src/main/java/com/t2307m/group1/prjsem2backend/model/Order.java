55100dccbeaabb12ff8939fdc28e7d8e8908359b
hongquan <danghongquanvtc1111@gmail.com>
2024-06-15 13:07:29 +0700

    update model.Order not complete

--- PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/Order.java
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/Order.java
@@ -1,13 +1,19 @@
 package com.t2307m.group1.prjsem2backend.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
+@Entity
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Account account;
 
 
 
