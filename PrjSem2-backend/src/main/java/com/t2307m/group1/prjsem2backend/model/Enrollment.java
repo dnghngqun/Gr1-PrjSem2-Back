a074d786993648fa773190db45b54b6ceca02cbe
hongquan <danghongquanvtc1111@gmail.com>
2024-06-15 12:15:43 +0700

    update model.Enrollment

--- PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/Enrollment.java
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/model/Enrollment.java
@@ -1,4 +1,67 @@
 package com.t2307m.group1.prjsem2backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
+
+@Entity
 public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Class aClass;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Account account;
+
+    private Date enrollmentDate;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(Class aClass, Account account, Date enrollmentDate) {
+        this.aClass = aClass;
+        this.account = account;
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Class getaClass() {
+        return aClass;
+    }
+
+    public void setaClass(Class aClass) {
+        this.aClass = aClass;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Date getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(Date enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
 }
