0998cb1104328d967fe18934b615d92f15d45d40
hongquan <danghongquanvtc1111@gmail.com>
2024-06-15 08:15:10 +0700

    create prj spring

---
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/PrjSem2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.t2307m.group1.prjsem2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjSem2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjSem2BackendApplication.class, args);
+    }
+
+}
---
+++ PrjSem2-backend/src/test/java/com/t2307m/group1/prjsem2backend/PrjSem2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2307m.group1.prjsem2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjSem2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
