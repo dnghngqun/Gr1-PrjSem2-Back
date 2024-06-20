17e7a5de5147d45fb1e12dc5fea04fbf5e08b863
hongquan <danghongquanvtc1111@gmail.com>
2024-06-20 20:57:46 +0700

    update luong instructor not complete

---
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/repositories/InstructorRepository.java
@@ -0,0 +1,11 @@
+package com.t2307m.group1.prjsem2backend.repositories;
+
+import com.t2307m.group1.prjsem2backend.model.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InstructorRepository extends JpaRepository<Instructor, Integer> {
+    Optional<Instructor> findByEmail(String email);
+}
+
---
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/service/InstructorService.java
@@ -0,0 +1,37 @@
+package com.t2307m.group1.prjsem2backend.service;
+
+import com.t2307m.group1.prjsem2backend.model.Instructor;
+import com.t2307m.group1.prjsem2backend.repositories.InstructorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class InstructorService {
+    private final InstructorRepository instructorRepository;
+
+    @Autowired
+    public InstructorService(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
+    }
+
+    public Instructor addInstructor(Instructor instructor){
+        return instructorRepository.save(instructor);
+    }
+
+    public Optional<Instructor> findIntructorByEmail(String email){
+        Optional<Instructor> instructorOptional = instructorRepository.findByEmail(email);
+        return instructorOptional;
+    }
+
+    public boolean updateInstructor(String email,Instructor instructor){
+        if (findIntructorByEmail(email).isEmpty()) return false;
+        Instructor newInstructor = findIntructorByEmail(email).get();
+        newInstructor.setName(instructor.getName());
+        newInstructor.setBio(instructor.getBio());
+        newInstructor.setPhoneNumber(instructor.getPhoneNumber());
+        instructorRepository.save(newInstructor);
+        return true;
+    }
+}
