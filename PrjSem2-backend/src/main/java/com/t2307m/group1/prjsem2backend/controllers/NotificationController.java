d7b8a20b7611811896f2f0360393922f24eff330
hongquan <danghongquanvtc1111@gmail.com>
2024-06-28 22:44:24 +0700

    update controller

--- PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/controllers/NotificationController.java
+++ PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/controllers/NotificationController.java
@@ -1,5 +1,9 @@
 package com.t2307m.group1.prjsem2backend.controllers;
 
-public class NotificationController {
+import com.t2307m.group1.prjsem2backend.service.NotificationService;
+import org.springframework.stereotype.Controller;
 
+@Controller
+public class NotificationController {
+    private NotificationService notificationService;
 }
