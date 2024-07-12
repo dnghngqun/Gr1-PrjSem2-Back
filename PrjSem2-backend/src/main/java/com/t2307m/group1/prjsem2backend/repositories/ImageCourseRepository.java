22525545ca20ccbfbcb314f7bba59de62d458bf0
dnghngqun <danghongquanvtc1111@gmail.com>
2024-07-13 05:09:18 +0700

    update remove file

--- PrjSem2-backend/src/main/java/com/t2307m/group1/prjsem2backend/repositories/ImageCourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.t2307m.group1.prjsem2backend.repositories;
-
-import com.t2307m.group1.prjsem2backend.model.ImageCourse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageCourseRepository  extends JpaRepository<ImageCourse, Integer> {
-}
