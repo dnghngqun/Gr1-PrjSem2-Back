b02a983a16489201c618b5cc050dee704f207b42
PHAMHOANG1902 <143236105+PHAMHOANG1902@users.noreply.github.com>
2024-07-16 10:48:02 +0700

    up db

--- DBPrj.sql
+++ DBPrj.sql
@@ -205,9 +205,9 @@ CREATE TABLE password_reset_tokens (
                                        expiration_date TIMESTAMP NOT NULL,
                                        FOREIGN KEY (account_id) REFERENCES Account(id)
 );
-INSERT INTO Account(username, password, fullname,imageAccount, birthday, email, phonenumber, role) values ('hongquan', 123456, 'Dang Hong Quan','https://i.imgur.com/jvskbNg.jpg', '1999-01-01','quandhth2304004@fpt.edu.vn','0383240511', 'customer');
-INSERT INTO Account(username, password, fullname,imageAccount ,birthday, email, phonenumber, role) values ('phamhoang', 123456, 'Pham Nhat Hoang','https://i.imgur.com/jvskbNg.jpg', '1999-01-01','hoangpnth2304021@fpt.edu.vn','0915298826', 'customer');
-INSERT INTO Account(username, password, fullname,imageAccount, birthday, email, phonenumber, role) values ('dothao', 123456, 'Do Thi Thao','https://i.imgur.com/jvskbNg.jpg', '1999-01-01','thaodtth2304010@fpt.edu.vn','0348279942', 'customer');
+INSERT INTO Account(username, password, fullname,imageAccount, birthday, email, phonenumber, role) values ('hongquan', 123456, 'Dang Hong Quan','https://i.imgur.com/1BhEejF.jpeg', '1999-01-01','quandhth2304004@fpt.edu.vn','0383240511', 'customer');
+INSERT INTO Account(username, password, fullname,imageAccount ,birthday, email, phonenumber, role) values ('phamhoang', 123456, 'Pham Nhat Hoang','https://i.imgur.com/1BhEejF.jpeg', '1999-01-01','hoangpnth2304021@fpt.edu.vn','0915298826', 'customer');
+INSERT INTO Account(username, password, fullname,imageAccount, birthday, email, phonenumber, role) values ('dothao', 123456, 'Do Thi Thao','https://i.imgur.com/1BhEejF.jpeg', '1999-01-01','thaodtth2304010@fpt.edu.vn','0348279942', 'customer');
 SELECT * FROM  Account;
 
 -- course TOEIC 2 Skills
